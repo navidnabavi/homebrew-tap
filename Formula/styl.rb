c95e86014a4989a93fc0c00abb73a36a762231e4
Navid Nabavi <navid92@gmail.com>
2026-05-22 04:06:10 +0330

    feat(styl): add Linux support to formula

--- Formula/styl.rb
+++ Formula/styl.rb
@@ -15,6 +15,17 @@ class Styl < Formula
     end
   end
 
+  on_linux do
+    on_arm do
+      url "https://github.com/navidnabavi/styl/releases/download/v#{version}/styl-v#{version}-aarch64-unknown-linux-gnu"
+      sha256 "021782fc50a514c7f966fa3ffbceb2b818aac7fda899ae151dddf1ff5b4295a0"
+    end
+    on_intel do
+      url "https://github.com/navidnabavi/styl/releases/download/v#{version}/styl-v#{version}-x86_64-unknown-linux-gnu"
+      sha256 "5748131cb23f1d09e496587471e9252f7db984ce93832886a97ef9842c340156"
+    end
+  end
+
   def install
     bin.install Dir["styl-*"].first => "styl"
   end
