e6ccc71e80fc13401590f9cac127156092195cbb
Navid Nabavi <navid92@gmail.com>
2026-06-03 23:44:34 +0330

    chore: bump styl to v0.0.5

--- Formula/styl.rb
+++ Formula/styl.rb
@@ -1,28 +1,28 @@
 class Styl < Formula
   desc "Linter, validator, and formatter for MapLibre GL / Mapbox GL style JSON"
   homepage "https://github.com/navidnabavi/styl"
-  version "0.0.4"
+  version "0.0.5"
   license "MIT"
 
   on_macos do
     on_arm do
       url "https://github.com/navidnabavi/styl/releases/download/v#{version}/styl-v#{version}-aarch64-apple-darwin"
-      sha256 "0a7a2deb42f8faad0a16fa695b3816e17aee23a73baf93f574e7bc014c3f8fdb"
+      sha256 "edec4e15dd7c8cdbecf6cd964e38bca20842b9048b84a76b6536faab4a590687"
     end
     on_intel do
       url "https://github.com/navidnabavi/styl/releases/download/v#{version}/styl-v#{version}-x86_64-apple-darwin"
-      sha256 "0be57f53fbf29d4cb9c3db5d76247e869e71f06c78911df5922d36904a3d882c"
+      sha256 "25fbc0a59daedbeb6fe410368edbb41ffed7557398cea45b677ede004a36a747"
     end
   end
 
   on_linux do
     on_arm do
       url "https://github.com/navidnabavi/styl/releases/download/v#{version}/styl-v#{version}-aarch64-unknown-linux-gnu"
-      sha256 "021782fc50a514c7f966fa3ffbceb2b818aac7fda899ae151dddf1ff5b4295a0"
+      sha256 "8e00ee84dc192b40da3fa95ae72fb6befaee4e80e15945c42748a6a84cc4889f"
     end
     on_intel do
       url "https://github.com/navidnabavi/styl/releases/download/v#{version}/styl-v#{version}-x86_64-unknown-linux-gnu"
-      sha256 "5748131cb23f1d09e496587471e9252f7db984ce93832886a97ef9842c340156"
+      sha256 "eb06dabc2e0814abd67a1199d4d80b5a9c37d049f01c45e1438334fb7b08fd23"
     end
   end
 
